6f45a7900885fd1c7d329e3a2990b3f979ad4f61
rahma <fijriatirahmaturrizqi@gmail.com>
2024-11-18 23:59:31 +0700

    update: percobaan 4

--- PengunjungCafe10.java
+++ PengunjungCafe10.java
@@ -1,12 +1,20 @@
 public class PengunjungCafe10 {
     public static void main(String[] args) {
         daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung(12, 25, 35);
     }
 
     public static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("daftar nama pengunjung:");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String nama : namaPengunjung) {
+            System.out.println("- " + nama);
+        }
+    }
+
+    public static void daftarPengunjung(int... umurPengunjung) {
+        System.out.println("daftar umur pengunjung:");
+        for (int umur : umurPengunjung) {
+            System.out.println("- " + umur);
         }
     }
 }
